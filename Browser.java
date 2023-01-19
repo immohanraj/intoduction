ec4787f1983f4645be1a3cfaaa430fe8f743270d
immohanraj <mohanraju1203@gmail.com>
2023-01-19 12:41:43 +0530

    Done

---
+++ Browser.java
@@ -0,0 +1,26 @@
+package browser.org;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Browser {
+
+	public static void main(String[] args) {
+
+		/*
+		 * WebDriver driver = new ChromeDriver(); driver.get("https://www.google.com/");
+		 */
+		
+		ChromeDriver obj = new ChromeDriver();
+		
+		
+		String a="Mohan";
+		
+	String s = new String ("Raj");	
+		System.out.println(s);
+
+	
+	
+	}
+
+}
